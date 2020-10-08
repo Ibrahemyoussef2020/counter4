22fbde6d95a8860c37cbbd61e4da38089e52b8af
Ibrahemyoussef2020 <ibrahem.youssef2020@hotmail.com>
2020-10-08 03:12:19 +0200

    in dark

--- j-test.js
+++ j-test.js
@@ -1,28 +1,31 @@
 
 var i = 0;
 
-function collect() {
+ document.getElementById("collect").onclick = function collect() {
 	
 document.getElementById('screen').innerHTML =  i++ ;
 
   
     
-}collect();
+}
 
 
- function restart(){
+
+ document.getElementById("restart").onclick = function restart(){
 
  	i = 0;
 
      document.getElementById('screen').innerHTML =  i ;
-}restart();
+}
 
 
 
 
-	function background() {
+document.getElementById("background").onclick = function background() {
    var element = document.body;
    element.classList.toggle("dark-mode");
+   document.getElementById("parent").classList.toggle("in-dark-mode");
+
 }
 
 
